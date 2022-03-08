22de3419f8172a4cb6d7b5d7fdaac49a1486ec98
dannyp0930 <dannyp0930@gmail.com>
2022-03-08 15:27:23 +0900

    REFACTOR: 부트스트랩 적용

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,11 +1,9 @@
-import { Button } from 'react-bootstrap'
 import React from 'react'
 
 export function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Button variant="primary">Primary</Button>{' '}
     </div>
   )
 };
