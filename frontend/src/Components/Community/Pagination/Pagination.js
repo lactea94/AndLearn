6b03505816035fcc948b8686b386a3e209c4e641
dannyp0930 <dannyp0930@gmail.com>
2022-03-15 00:26:06 +0900

    REFACTOR: Pagination 간결화

--- frontend/src/Components/Community/Pagination/Pagination.js
+++ frontend/src/Components/Community/Pagination/Pagination.js
@@ -3,53 +3,30 @@ import { Pagination as Page }  from "react-bootstrap";
 export default function Pagination({ total, limit, page, setPage}) {
   const numPages = Math.ceil(total / limit);
   const Pages = () => {
-    if (page < 4) {
-      const array = [1, 2, 3, 4, 5]
-      return (
-        <>
-          {array.map(i => (
-            <Page.Item
-              key={i}
-              onClick={() => setPage(i)}
-              active={(page === i)}
-            >
-              {i}
-            </Page.Item>
-          ))}
-          <Page.Ellipsis />
-        </>
-    )} else if (page > numPages - 3) {
-      const array = [numPages - 4, numPages - 3, numPages - 2, numPages - 1, numPages]
-        return (
-          <>
-            <Page.Ellipsis />
-            {array.map(i => (
-              <Page.Item
-                key={i}
-                onClick={() => setPage(i)}
-                active={(page === i)}
-                >
-                {i}
-              </Page.Item>
-            ))}
-          </>
-    )} else {
-      const array = [page - 2, page - 1, page , page + 1]
-      return (
-        <>
-          <Page.Ellipsis />
-          {array.map(i => (
-            <Page.Item
-            key={i}
-            onClick={() => setPage(i)}
-            active={(page === i)}
-            >
-              {i}
-            </Page.Item>
-          ))}
-          <Page.Ellipsis />
-        </>
-    )}}
+    const array = () => {
+      if (page < 4) {
+        return [1, 2, 3, 4, 5]
+      } else if (page > numPages - 3) {
+        return [numPages - 4, numPages - 3, numPages - 2, numPages - 1, numPages]
+      } else {
+        return [page - 2, page - 1, page , page + 1]
+      }
+    }
+    return (
+      <>
+        {(Page > 3) && <Page.Ellipsis />}
+        {array().map(i => (
+          <Page.Item
+          key={i}
+          onClick={() => setPage(i)}
+          active={(page === i)}
+          >
+            {i}
+          </Page.Item>
+        ))}
+        {(page < numPages - 2) && <Page.Ellipsis />}
+      </>
+    )}
 
   return (
     <Page className="justify-content-center">
