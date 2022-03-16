d4274e8de1eab801847179bad275e619d91e157f
kimcookie <jor9511@naver.com>
2022-03-16 13:37:36 +0900

    Feat : Profile main button 추가

--- frontend/src/Components/Profile/Profile.js
+++ frontend/src/Components/Profile/Profile.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
 import { useParams } from 'react-router-dom'
 
 export function Profile() {
   const { userId } = useParams();
 
   return (
-    <div>{userId} Profile</div>
+    <div>
+      <Container>
+        {userId} Profile
+        <Row>
+          <Col xs={2} bg-black>
+            Blank
+          </Col>
+          <Col xs={2}>
+            Image
+          </Col>
+          <Col xs={6}>
+            Profile
+          </Col>
+          <Col xs={2}>
+            Blank
+          </Col>
+        </Row>
+        <hr/>
+        <Row>
+          bye
+        </Row>
+      </Container>
+    </div>
   )
 };
