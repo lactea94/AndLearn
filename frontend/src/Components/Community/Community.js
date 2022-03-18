ededa716fa71e5940ea35b2cfadf6e48e3557d69
dannyp0930 <dannyp0930@gmail.com>
2022-03-18 13:50:32 +0900

    FIX: 페이지 당 게시글 수 변경시 페이지 오류 수정

--- frontend/src/Components/Community/Community.js
+++ frontend/src/Components/Community/Community.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Col, Container, DropdownButton, Dropdown, Row, Table, OverlayTrigger, Tooltip } from 'react-bootstrap'
+import { Col, Container, Row, Table, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import Pagination from './Pagination/Pagination';
 
