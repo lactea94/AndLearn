6c2cb99c248674d3bc71d31a2c60a16286e47edc
dannyp0930 <dannyp0930@gmail.com>
2022-03-22 11:56:08 +0900

    STYLE: nav hover and focus

--- frontend/src/Common/Navigation/Navigation.js
+++ frontend/src/Common/Navigation/Navigation.js
@@ -1,12 +1,11 @@
 import { useState } from "react"
 import { Navbar, Container, Nav, Offcanvas } from "react-bootstrap"
-import { NavLink } from "react-router-dom"
 import * as S from "./Style"
 
 export function Navigation() {
   // 임시로 사용하는 프로필 접근 닉네임? 아이디?
   const userId = 'kimcookie'
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
 
   return (
     <S.MyNavbar expand={false}>
@@ -16,22 +15,24 @@ export function Navigation() {
         placement="start"
       >
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title><S.NavItem as={NavLink} to="/">Home</S.NavItem></Offcanvas.Title>
+          <Offcanvas.Title>
+            <S.NavItem to="/">Home</S.NavItem>
+          </Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           <Nav className="justify-content-end flex-grow-1 pe-3">
             { isAuthenticated ? 
               <>
-                <S.NavItem as={NavLink} to="/">Home</S.NavItem>
-                <S.NavItem as={NavLink} to={`profile/${userId}/content`}>Profile</S.NavItem>
-                <S.NavItem as={NavLink} to="learn">Learn</S.NavItem>
-                <S.NavItem as={NavLink} to="community">Community</S.NavItem>
+                <S.NavItem to="/">Home</S.NavItem>
+                <S.NavItem to={`profile/${userId}/content`}>Profile</S.NavItem>
+                <S.NavItem to="learn">Learn</S.NavItem>
+                <S.NavItem to="community">Community</S.NavItem>
               </>
             :
               <>
-                <S.NavItem as={NavLink} to="/">Home</S.NavItem>
-                <S.NavItem as={NavLink} to="login">LogIn</S.NavItem>
-                <S.NavItem as={NavLink} to="signup">SignUp</S.NavItem>
+                <S.NavItem to="/">Home</S.NavItem>
+                <S.NavItem to="login">Login</S.NavItem>
+                <S.NavItem to="signup">Signup</S.NavItem>
               </>
             }
           </Nav>
