e64862a883faa58e3a252dc7dd7aa478aefd5d6d
pjyc17 <pjyc17@gmail.com>
2022-03-23 17:23:04 +0900

    FEAT: 로그인 중복검사 & API front 통신 성공

--- backend/backend/src/main/java/com/ssafy/api/response/UserRes.java
+++ backend/backend/src/main/java/com/ssafy/api/response/UserRes.java
@@ -1,12 +1,13 @@
 package com.ssafy.api.response;
 
-import com.ssafy.common.model.response.BaseResponseBody;
 import com.ssafy.db.entity.User;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Optional;
 
 /**
  * 회원 본인 정보 조회 API ([GET] /api/v1/users/me) 요청에 대한 응답값 정의.
