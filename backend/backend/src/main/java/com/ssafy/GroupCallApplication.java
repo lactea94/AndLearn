7fae16cc0558c3e674c78b505e74dee382802f31
박상훈 <dannyp0930@daum.net>
2022-03-27 08:24:02 +0000

    Update GroupCallApplication.java

--- backend/backend/src/main/java/com/ssafy/GroupCallApplication.java
+++ backend/backend/src/main/java/com/ssafy/GroupCallApplication.java
@@ -6,13 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.boot.system.ApplicationPidFileWriter;
+
 
 import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 public class GroupCallApplication {
 	public static void main(String[] args) {
-        SpringApplication.run(GroupCallApplication.class, args);
+        // SpringApplication.run(GroupCallApplication.class, args);
+        SpringApplication app = new SpringApplication(GroupCallApplication.class);
+        app.addListeners(new ApplicationPidFileWriter()); // pid 파일을 생성하는 writer 등록
+        app.run(args);
     }
 
     @Bean
