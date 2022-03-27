d3fe33aba38aab9fdb6915890cd89e8b16255f04
박상훈 <dannyp0930@daum.net>
2022-03-27 08:45:07 +0000

    FIX: url

--- backend/AIserver/AIserver/urls.py
+++ backend/AIserver/AIserver/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/aiserver/', include('predictions.urls')),
+    path('aiserver/', include('predictions.urls')),
 ]
