6c9c97b23039c4cacd22b0a84b993049a44e495e
박상훈 <dannyp0930@daum.net>
2022-03-27 08:28:37 +0000

    Update GroupCallApplication.java

--- backend/backend/src/main/java/com/ssafy/GroupCallApplication.java
+++ backend/backend/src/main/java/com/ssafy/GroupCallApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 
 import java.nio.charset.StandardCharsets;
