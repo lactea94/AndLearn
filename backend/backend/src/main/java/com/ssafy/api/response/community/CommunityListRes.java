18e1519bdb211dcb053e944dc1411555e917d479
Byeongwan <quddhs4694@naver.com>
2022-03-30 01:27:56 +0900

    FEAT:공지사항 CRUD

--- backend/backend/src/main/java/com/ssafy/api/response/community/CommunityListRes.java
+++ backend/backend/src/main/java/com/ssafy/api/response/community/CommunityListRes.java
@@ -1,6 +1,7 @@
 package com.ssafy.api.response.community;
 
 import com.ssafy.db.entity.Community;
+import com.ssafy.db.entity.User;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -12,21 +13,29 @@ import java.time.LocalDateTime;
 @Setter
 @ApiModel("CommunityListGetResponse")
 public class CommunityListRes {
+    private Long userId;
+    private String nickname;
+    private String imgUrl;
+
     @ApiModelProperty(name = "seq", example = "1")
-    Long id;
+    private Long id;
 
     @ApiModelProperty(name = "게시판 제목", example = "this is community title")
-    String title;
+    private String title;
 
     @ApiModelProperty(name = "게시판 생성 날짜", example = "2022-03-28 15:20:24.340005")
-    LocalDateTime createdAt;
+    private LocalDateTime createdAt;
     @ApiModelProperty(name = "게시판 생성 날짜", example = "2022-03-28 15:20:24.340005")
-    LocalDateTime updatedAt;
+    private LocalDateTime updatedAt;
 
     public CommunityListRes(Community entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.createdAt = entity.getCreatedDate();
         this.updatedAt = entity.getUpdatedDate();
+        User user = entity.getUser();
+        this.nickname = user.getNickname();
+        this.userId = user.getId();
+        this.imgUrl = user.getImage_url();
     }
 }
