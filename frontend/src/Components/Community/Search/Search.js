656cacdabcb58427441427e534a5cf40b9b974fc
dannyp0930 <dannyp0930@gmail.com>
2022-03-29 15:31:02 +0900

    STYLE: 색상 및 배치 변경

--- frontend/src/Components/Community/Search/Search.js
+++ frontend/src/Components/Community/Search/Search.js
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import { Col } from "react-bootstrap";
-import * as S from "./Style"
+import { MyButton } from "styles/Button";
+import { Input } from "styles/Input";
+import { Select } from "styles/Select";
 
-export function Search({ setSearchTerm }) {
+export function Search({ setSearchText, setSearchCategory, setPage }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("title");
+
   return (
     <Col xs={6}>
-      <S.Control
-        placeholder="제목을 검색하세요"
-        onChange={(e) => {
-          setSearchTerm(e.target.value)
+      <Select
+        onChange={e => {
+          e.preventDefault();
+          setCategory(e.target.value);
         }}
-        />
+      >
+        <option value="title">제목</option>
+        <option value="body">내용</option>
+      </Select>
+      <Input
+        type="text"
+        value={text}
+        onChange={(e) => {
+          e.preventDefault();
+          setText(e.target.value);
+        }}
+      />
+      <MyButton
+        onClick={() => {
+          setSearchText(text);
+          setSearchCategory(category);
+          setText("");
+          setPage(1);
+        }}
+      >
+        검색
+      </MyButton>
     </Col>
   )
 }
--- frontend/src/Components/Community/Search/Style.js
+++
@@ -1,9 +0,0 @@
-import { Form } from "react-bootstrap";
-import styled from "styled-components";
-
-export const Control = styled(Form.Control)`
-  &:focus {
-    box-shadow: none;
-    border: solid 1px #58C063
-  }
-`;
