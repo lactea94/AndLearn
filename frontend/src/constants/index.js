da5b752cdc20650e7cd07fcac5667ae475f453f9
dannyp0930 <dannyp0930@gmail.com>
2022-03-30 15:15:00 +0900

    FIX: 머지 충돌

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -4,9 +4,5 @@
 // 배포
 export const API_BASE_URL = 'https://j6c201.p.ssafy.io';
 
-<<<<<<< HEAD
-export const ACCESS_TOKEN = 'accesstoken';
-=======
 export const ACCESS_TOKEN = 'accesstoken'
->>>>>>> 483e0cb163372c7de97cff609fc3d787b8dc63af
 
