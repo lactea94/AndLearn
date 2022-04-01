332932f43d3a0a3cbb1407510319d7d3d070280e
Byeongwan <quddhs4694@naver.com>
2022-04-01 12:36:29 +0900

    FIX:Settings.py CORS

--- backend/AIserver/AIserver/settings.py
+++ backend/AIserver/AIserver/settings.py
@@ -161,3 +161,15 @@ CORS_ALLOW_METHODS = [
     'POST',
     'PUT',
 ]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
