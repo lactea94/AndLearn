835be0dfc85ffb69db894b6a3780fb89e4308c8b
seungkyu-lee <leesk3732@naver.com>
2022-04-02 23:33:49 +0900

    FIX: 런'

--- frontend/src/Components/Learn/ImageUpload.js
+++ frontend/src/Components/Learn/ImageUpload.js
@@ -3,12 +3,13 @@ import { djangoInstance } from 'api/index'
 import { MyButton } from 'styles/Button'
 //import Dropzone from 'react-dropzone'
 
-export function ImageUpload({ setFileImage, setImageId, next, setKeyDjango }) {
+export function ImageUpload({ setFileImage, next, setKeyDjango, setWords }) {
   const [image, setImage] = useState('')
   const api = djangoInstance()
 
   function onLoad(e) {
     setImage(e.target.files[0])
+    console.log(e.target.files[0])
     setFileImage(URL.createObjectURL(e.target.files[0]))
   }
 
@@ -16,17 +17,21 @@ export function ImageUpload({ setFileImage, setImageId, next, setKeyDjango }) {
     next()
     const formData = new FormData()
     formData.append('file', image)
+
     api
       .post('image', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
+          'Access-Control-Allow-Credentials': true,
         },
       })
       .then((res) => {
         console.log(res)
         setImage(res.data)
-        setImageId(res.data.id)
-        setKeyDjango()
+        setKeyDjango(res.data.id)
+        setWords(res.data.words)
+        console.log(res.data.words)
+
         next()
       })
       .catch((error) => {
--- frontend/src/Components/Learn/Learn.js
+++ frontend/src/Components/Learn/Learn.js
@@ -4,13 +4,15 @@ import { AudioRecord } from './AudioRecord'
 import { GrammarlyEditor } from './GrammarlyEditor'
 import plusDefault from './plusDefault.png'
 import { apiInstance } from 'api/index'
+import { MyButton } from 'styles/Button.js'
 
 export function Learn() {
   const api = apiInstance()
   const [fileImage, setFileImage] = useState(plusDefault)
   const [imageId, setImageId] = useState('')
   const [stage, setStage] = useState(0)
-  const [keyDjango, setKeyDjango] = useState('')
+  const [keyDjango, setKeyDjango] = useState(2)
+  const [words, setWords] = useState([])
 
   const [audioUrl1, setAudioUrl1] = useState()
   const [aud1, setAud1] = useState()
@@ -20,10 +22,9 @@ export function Learn() {
 
   const [script1, setScript1] = useState('')
   const [script2, setScript2] = useState('')
-
+  const recommendWord = words.map((word) => <p key={word.id}>{word}</p>)
   function next() {
     setStage(stage + 1)
-    console.log(stage)
   }
   function onCheck(e) {
     console.log(e.target.value)
@@ -32,12 +33,22 @@ export function Learn() {
   function onSubmit() {
     const formData = new FormData()
     formData.append('file', aud1)
+    formData.append('file2', aud2)
+    
+    const data = {
+      score: '5.0',
+      words: words,
+      sentences: [script1, script2],
+    }
+    formData.append(
+      'learnPostReq',
+      new Blob([JSON.stringify(data)], { type: 'application/json' })
+    )
 
     api
       .post(`learn/${keyDjango}`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
-          'Access-Control-Allow-Credentials': true,
         },
       })
       .then((res) => {
@@ -54,12 +65,13 @@ export function Learn() {
         {stage === 0 && (
           <ImageUpload
             setFileImage={setFileImage}
-            setImageId={setImageId}
             next={next}
             setKeyDjango={setKeyDjango}
+            setWords={setWords}
           />
         )}
       </div>
+      <div>{recommendWord}</div>
       <div>
         {stage >= 1 && (
           <>
@@ -69,12 +81,17 @@ export function Learn() {
               setAudioUrl1={setAudioUrl1}
               setAud1={setAud1}
             />
+
             {aud1 && (
               <>
                 <textarea value={script1} onChange={onCheck}>
                   {script1}
                 </textarea>
-                <audio controls src={aud1} controlsList="nodownload"></audio>
+                <audio
+                  controls
+                  src={audioUrl1}
+                  controlsList="nodownload"
+                ></audio>
               </>
             )}
           </>
@@ -97,12 +114,15 @@ export function Learn() {
               setAud1={setAud2}
             />
             {aud2 && (
-              <audio controls src={aud2} controlsList="nodownload"></audio>
+              <audio controls src={audioUrl2} controlsList="nodownload"></audio>
             )}
           </>
         )}
       </div>
-      <div>{stage >= 4 && <div>다음</div>}</div>
+      <div>{stage >= 4 && <MyButton onClick={onSubmit}>다음</MyButton>}</div>
+      <div>
+        {stage >= 5 && <MyButton onClick={onSubmit}>전송완료</MyButton>}
+      </div>
     </div>
   )
 }
