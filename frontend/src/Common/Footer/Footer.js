e21a907a97375befcf5017f83a2f981f1636cfca
Byeongwan <quddhs4694@naver.com>
2022-04-04 16:18:10 +0900

    STYLE: footer 문구 및 디자인 변경

--- frontend/src/Common/Footer/Footer.js
+++ frontend/src/Common/Footer/Footer.js
@@ -3,9 +3,10 @@ import * as S from "./Style"
 export const Footer = () => {
   return (
     <S.Content>
+      <S.bar />
       Andlearn<br/>
       광주광역시 광산구 하남산단6번로 107, SSAFY 2층<br/>
-      © 2022 ToMakeMattersBetter. All Rights Reserved.
+      © 2022 A.1. All Rights Reserved.
     </S.Content>
   )
 }
--- frontend/src/Common/Footer/Style.js
+++ frontend/src/Common/Footer/Style.js
@@ -4,3 +4,10 @@ export const Content = styled.div`
   font-size: 0.3rem;
   margin: 1rem 0 2rem;
 `
+
+export const bar = styled.hr`
+  width: 80%;
+  margin-top: 5rem;
+  margin-left: 10%;
+  margin-bottom: 2rem;
+`
