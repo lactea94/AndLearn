6798615bd328cfe57f47d94cc9a19bcaf56108c6
Byeongwan <quddhs4694@naver.com>
2022-04-05 20:11:28 +0900

    STYLE:반응형 적용완료

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -30,42 +30,41 @@ export function Home() {
       <S.HeaderInfo>▼ 밑으로 내려보세요!</S.HeaderInfo>
       <S.Body>
         <br/>
-        <S.Content>
+        <S.ServiceContent>
           <S.ServiceTitle><span style={{ color: '#58C063' }}>AndLearn</span>이란??</S.ServiceTitle>
           <S.ServiceText>영어가 낯선사람을 위한 언어학습 서비스 입니다.</S.ServiceText>
           <S.ServiceText>외국어를 학습에 있어 가장 효과적인 방법들을 바탕으로 서비스를 제작하였습니다. </S.ServiceText>
           <S.ServiceText>아래의 과정을 통해 언어 습득의 3가지 관점에 따라 언어를 학습하는 방법을 제공합니다.</S.ServiceText>
-          <S.ServiceEmphasizeText><span style={{ color: '#58C063' }}>그럼 저희 서비스 한번 보실래요?</span></S.ServiceEmphasizeText>
-        </S.Content>
+        </S.ServiceContent>
+        <S.EmphasizeText><span style={{ color: '#58C063' }}>그럼 저희 서비스 한번 보실래요?</span></S.EmphasizeText>
         <S.Image src="/images/example.jpg"/>
-        <S.Content>
-          <Col xs={8}>
+        <S.FirstStepContent>
+          <Col xs={12} sm={12} xl={8}>
             <S.ContentTitle>어떻게 묘사하실 건가요??</S.ContentTitle>
             <br/>
             <S.ContentText>이 사진을 보고 바로 영어로 말할 수 있으셨나요?</S.ContentText>
             <S.ContentEmphasizeText><span style={{ color: '#58C063' }}>인지적 관점에 따르면</span></S.ContentEmphasizeText>
             <S.ContentText>이미지를 보고 표현하는 과정을 통해 자연스럽게 언어를 학습하게 됩니다!</S.ContentText>
           </Col>
-          <Col xs={4}>
-            <S.Image src="/images/allu-1.svg"/>
+          <Col xs={12} sm={12} xl={4}>
+            <S.Mascot src="/images/allu-1.svg"/>
           </Col>
-        </S.Content>
-        <S.Content>
-          <Col xs={12}>
-          <S.Image src="asdf.png" alt='단어가 나오고 녹음하는 부분 이미지'></S.Image></Col>
-          <Col xs={4}>
-            <S.Image src="/images/allu-2.svg"/>
+        </S.FirstStepContent>
+        <S.Image src="asdf.png" alt='단어가 나오고 녹음하는 부분 이미지'></S.Image>
+        <S.SecondStepContent>
+          <Col xs={12} sm={12} xl={4}>
+            <S.Mascot src="/images/allu-2.svg"/>
           </Col>
-          <Col xs={8}>
+          <Col xs={12} sm={12} xl={8}>
             <S.ContentTitle>AI가 보여주는 단어들로 새 문장을 만들어봐요!</S.ContentTitle>
             <S.ContentEmphasizeText><span style={{ color: '#58C063' }}>새로운 단어를 내것으로 만드는 구성적 관점</span>과</S.ContentEmphasizeText> 
             <S.ContentEmphasizeText><span style={{ color: '#58C063' }}>반복과 모방을 통한 행동적 관점</span>의 학습 방법을 제공합니다.</S.ContentEmphasizeText> 
           </Col>
-        </S.Content>
-        <Col>
-            <S.RecommandEmphasizeText><span style={{ color: '#58C063' }}>AndLearn</span>은 영어가 낯선 분들을 위한 서비스 입니다.</S.RecommandEmphasizeText>
-            <S.RecommandEmphasizeText>사진을 보고 자유롭게 연습해 보세요.</S.RecommandEmphasizeText>
-        </Col>
+        </S.SecondStepContent>
+        <S.ThirdStepContent>
+          <S.RecommandEmphasizeText><span style={{ color: '#58C063' }}>AndLearn</span>은 영어가 낯선 분들을 위한 서비스 입니다.</S.RecommandEmphasizeText>
+          <S.RecommandEmphasizeText>사진을 보고 자유롭게 연습해 보세요.</S.RecommandEmphasizeText>
+        </S.ThirdStepContent>
         <S.Content>
           <Carousel>
             {Carousels}
--- frontend/src/Components/Home/Style.js
+++ frontend/src/Components/Home/Style.js
@@ -6,34 +6,33 @@ export const Contents = styled(Container)`
   font-family: BMEULJIRO;
   font-size: 0.7rem;
 
-  @media ( min-width: 375px ) {
-    font-size: 1.0rem;
-  }
-
-  @media ( min-width: 768px ) {
+  @media ( min-width: 767px ) {
     font-size: 1.3rem;
     width: 80%;
   }
-
-  @media ( min-width: 1280px ) {
+  @media ( min-width: 1199px ) {
+    font-size: 1.5rem;
+    width: 65%;
+  }
+  @media ( min-width: 1439px ) {
     font-size: 1.5rem;
     width: 65%;
   }
 `
 export const HeaderText = styled.div`
-  font-size: 1.6rem;
   align-items: center;
-  margin-top : 5rem;
+  font-size: 1.6rem;
+  margin-top : 3rem;
 
-  @media ( min-width: 375px ) {
-    font-size: 2.2rem;
-    margin-top : 3rem;
-  }
-  @media ( min-width: 768px ) {
+  @media ( min-width: 767px ) {
     font-size: 2.8rem;
   }
-  @media ( min-width: 1280px ) {
+  @media ( min-width: 1199px ) {
     font-size: 3.2rem;
+  }
+  @media ( min-width: 1439px ) {
+    font-size: 3.6rem;
+    margin-top : 2.5rem;
   }
   &:hover {
     color: #58C063;
@@ -42,39 +41,59 @@ export const HeaderText = styled.div`
 `
 
 export const Header = styled(Row)`
-  padding-top: 2rem;
   align-items: center;
+  padding-top: 6rem;
 
-  @media ( min-width: 420px ) {
+  @media ( min-width: 767px ) {
     padding-top : 10rem; 
+  }
+  @media ( min-width: 1199px ) {
+    padding-top : 3rem;
+  }
+  @media ( min-width: 1439px ) {
+    padding-top : 2rem;
+  }
+`
+
+export const Image = styled.img`
+  width: 80%;
+  margin-top: 3rem;
+  margin-bottom: 2.5rem;
+
+  @media( min-width : 767px ){
+    width: 100%;
+  }
+  @media ( min-width: 1199px ) {
+    width: 90%;
   }
 `
 
 export const HeaderInfo = styled.div`
-  font-size: 1.0rem;
   text-align: end;
-  margin-top : 4rem;
+  margin-top : 8rem;
   margin-right: 1rem;
+  font-size: 1.0rem;
 
-  @media ( min-width: 420px ) {
-    font-size: 1.1rem;
-  }
-
-  @media ( min-width: 820px ) {
+  @media ( min-width: 767px ) {
     font-size: 1.3rem;
+    margin-top : 11rem;
+    margin-right: 1rem;
   }
-  @media ( min-width: 1280px ) {
+  @media ( min-width: 1199px ) {
     font-size: 1.7rem;
+    margin-top : 3rem;
+    margin-right: 1rem;
+  }
+  @media ( min-width: 1439px ) {
+    font-size: 2.0rem;
+    margin-top : 3rem;
+    margin-right: 1rem;
   }
 
   &:hover {
     color: #FFDD74;
     transition-duration : 0.3s;
   }
-`
-
-export const Image = styled.img`
-  width: 90%;
 `
 
 export const Body = styled(Row)`
@@ -86,22 +105,51 @@ export const Content = styled(Row)`
   margin: 7rem 2rem;
   justify-content: center;
   align-items: center;
+
+  @media( min-width: 767px) {
+    margin: 7rem 1rem;
+  }
+
+  @media ( min-width: 1199px ) {
+    margin: 8rem 2rem;
+  }
+`
+
+export const ServiceContent = styled(Row)`
+  margin: 7rem 2rem;
+  padding : 4rem 1rem;
+  justify-content: center;
+  align-items: center;
+
+  @media( min-width: 767px) {
+    margin: 10rem 2rem;
+    padding: 5rem 0.5rem 8rem 0.5rem;
+  }
+  @media ( min-width: 1199px ) {
+    margin: 10rem 2rem;
+    padding: 5rem 0.5rem 8rem 0.5rem;
+  }
+  @media ( min-width: 1439px ) {
+    margin: 8rem 2rem;
+    padding: 4rem 0.5rem 10rem 0.5rem;
+  }
+
 `
 
 export const ServiceTitle = styled.h1`
   text-align: start;
   margin-bottom: 2rem;
-  font-size: 3rem;
+  font-size: 2.8rem;
   
-  @media ( min-width: 420px ) {
-    font-size: 3.5rem;
-  }
-
-  @media ( min-width: 820px ) {
+  @media ( min-width: 767px ) {
     font-size: 4rem;
+    margin-bottom: 2rem;
   }
-  @media ( min-width: 1280px ) {
+  @media ( min-width: 1199px ) {
     font-size: 5rem;
+  }
+  @media ( min-width: 1439px ) {
+    font-size: 6rem;
   }
 `
 
@@ -110,39 +158,96 @@ export const ServiceText = styled.div`
   margin-bottom: 0.5rem;
   font-size: 1rem;
 
-  @media ( min-width: 420px ) {
-    font-size: 1.1rem;
-  }
-
-  @media ( min-width: 820px ) {
+  @media ( min-width: 767px ) {
     font-size: 1.3rem;
+    margin-bottom: 0.5rem;
   }
-
-  @media ( min-width: 1280px ) {
-    font-size: 1.7rem;
+  @media ( min-width: 1199px ) {
+    font-size: 1.35rem;
+    margin-bottom: 0.5rem;
+  }
+  @media ( min-width: 1439px ) {
+    font-size: 2rem;
+    margin-bottom: 0.5rem;
   }
 `
 
-export const ServiceEmphasizeText = styled.h1`
-  text-align : start;
-  margin: 7rem 3rem 0rem;
+export const EmphasizeText = styled.div`
+  text-align : center;
+  font-size : 1.5rem;
+  padding-top : 1.3rem;
+  padding-bottom : 2rem;
+
+  @media ( min-width: 767px ) {
+    font-size : 2.5rem;
+    padding-top : 1.7rem;
+    padding-bottom : 2.4rem;
+  }
+  @media ( min-width: 1199px ) {
+    font-size : 2.6rem;
+    padding-top : 1.3rem;
+    padding-bottom : 2rem;
+  }
+  @media ( min-width: 1439px ) {
+    font-size : 3rem;
+    padding-top : 1.3rem;
+    padding-bottom : 6rem;
+  }
+`
+
+export const FirstStepContent = styled(Row)`
+  justify-content: center;
+  align-items: center;
+  margin: 7rem 2rem;
+  padding : 4rem 1rem;
+
+  @media( min-width: 767px) {
+    margin: 7rem 2rem;
+    padding : 4rem 1rem;
+  }
+  @media ( min-width: 1199px ) {
+    margin: 6rem 2rem;
+    padding : 8rem 0rem;
+  }
+  @media ( min-width: 1439px ) {
+    margin: 4rem 2rem;
+    padding : 7rem 0rem;
+  }
+`
+
+export const SecondStepContent = styled(Row)`
+  justify-content: center;
+  align-items: center;
+  margin: 7rem 2rem;
+  padding : 4rem 1rem;
+
+  @media( min-width: 767px) {
+    margin: 7rem 2rem;
+    padding : 4rem 1rem;
+  }
+  @media ( min-width: 1199px ) {
+    margin: 6rem 2rem;
+    padding : 8rem 0rem;
+  }
+  @media ( min-width: 1439px ) {
+    margin: 7rem 2rem;
+    padding : 10rem 0rem;
+  }
 `
 
 export const ContentTitle = styled.h1`
   text-align: center;
   margin-bottom: 2rem;
-  font-size: 1.8rem;
+  font-size: 1.97rem;
   
-  @media ( min-width: 420px ) {
-    font-size: 2.1rem;
+  @media ( min-width: 767px ) {
+    font-size: 3.4rem;
   }
-
-  @media ( min-width: 820px ) {
-    font-size: 2.3rem;
+  @media ( min-width: 1199px ) {
+    font-size: 3.0rem;
   }
-  
-  @media ( min-width: 1280px ) {
-    font-size: 2.5rem;
+  @media ( min-width: 1439px ) {
+    font-size: 2.6rem;
   }
 `
 export const ContentText = styled.div`
@@ -150,16 +255,15 @@ export const ContentText = styled.div`
   margin-bottom: 0.5rem;
   font-size : 0.9rem;
 
-  @media ( min-width: 420px ) {
-    font-size: 1.1rem;
-  }
-
-  @media ( min-width: 820px ) {
-    font-size: 1.3rem;
-  }
-  
-  @media ( min-width: 1280px ) {
+  @media ( min-width: 767px ) {
     font-size: 1.6rem;
+    margin-bottom: 0.6rem;
+  }
+  @media ( min-width: 1199px ) {
+    font-size: 1.4rem;
+  }
+  @media ( min-width: 1439px ) {
+    font-size: 1.7rem;
   }
 `
 
@@ -168,33 +272,66 @@ export const ContentEmphasizeText = styled.div`
   margin-bottom: 0.5rem;
   font-size: 1.1rem;
 
-  @media ( min-width: 420px ) {
-    font-size: 1.3rem;
+  @media ( min-width: 767px ) {
+    font-size: 1.9rem;
+  }
+  @media ( min-width: 1199px ) {
+    font-size: 1.7rem;
+  }
+  @media ( min-width: 1439px ) {
+    font-size: 1.5em;
+  }
+`
+export const Mascot = styled.img`
+  width: 80%;
+  margin-top: 3rem;
+  margin-bottom: 2.5rem;
+
+  @media( min-width : 767px ){
+    width: 50%;
+    padding-top: 3rem;
+    padding-bottom: 4rem;
   }
 
-  @media ( min-width: 820px ) {
-    font-size: 1.5rem;
+  @media ( min-width: 1199px ) {
+    width: 100%;
+    padding-top: 3rem;
+    padding-bottom: 4rem;
   }
-  
-  @media ( min-width: 1280px ) {
-    font-size: 1.9rem;
+`
+
+export const ThirdStepContent = styled(Row)`
+  justify-content: center;
+  align-items: center;
+  margin: 7rem 2rem;
+  padding : 4rem 0rem;
+
+  @media( min-width: 767px) {
+    margin: 8rem 2rem;
+    padding : 10rem 0rem;
+  }
+  @media ( min-width: 1199px ) {
+    margin: 8rem 2rem;
+    padding : 10rem 0rem;
+  }
+  @media ( min-width: 1199px ) {
+    margin: 9rem 2rem;
+    padding : 11rem 0rem;
   }
 `
 
 export const RecommandEmphasizeText = styled.div`
   text-align : center;
   margin-bottom: 0.5rem;
-  font-size: 1.5rem;
+  font-size: 1.05rem;
 
-  @media ( min-width: 420px ) {
-    font-size: 1.7rem;
+  @media ( min-width: 767px ) {
+    font-size: 1.8rem;
   }
-
-  @media ( min-width: 820px ) {
-    font-size: 1.9rem;
-  }
-  
-  @media ( min-width: 1280px ) {
+  @media ( min-width: 1199px ) {
     font-size: 2.3rem;
+  }
+  @media ( min-width: 1199px ) {
+    font-size: 2.7rem;
   }
 `
