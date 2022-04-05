c34cd42133b30d8886f46e30c9518a1085294541
pjyc17 <pjyc17@gmail.com>
2022-04-05 16:24:21 +0900

    FIX: link_btn to nav_link isactive color change, size of font, detail reacting web page

--- frontend/src/Components/Profile/Style.js
+++ frontend/src/Components/Profile/Style.js
@@ -7,3 +7,25 @@ export const UserImg = styled.img`
   overflow: hidden;
   object-fit: cover;
 `
+
+export const BtnAction = styled.button`
+  border: none;
+  background-color: #58C063;
+  &:hover {
+    background-color: #915450;
+  }
+`
+export const navLinkStyle = ({ isActive }) => (
+  {
+    textDecoration: "none",
+    backgroundColor: isActive ? "#FFDD74" : "#58C063",
+    color: "white",
+    fontSize: "1rem",
+    fontFamily: "Maru Buri",
+    "padding-right": "0.8rem",
+    "padding-left": "0.8rem",
+    "padding-top": "0.53rem",
+    "padding-bottom": "0.53rem",
+    "border-radius": "0.31rem"
+  }
+)
