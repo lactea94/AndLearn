d8afef32e426ce04f9b2e0de9559ff9dbbfc7bd5
dannyp0930 <dannyp0930@gmail.com>
2022-04-06 21:06:01 +0900

    FEAT: enter시 회원가입

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Col, Form, Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import { MyButton } from 'styles/Button';
 import { userInstance } from 'api/index';
 import * as S from './Style';
@@ -48,40 +48,37 @@ export function Login() {
   }
 
   return (
-    <S.Contents>
-      <Form onKeyPress={handleKeyPress}>
-        <Row className="justify-content-center">
-          <Col style={{marginBottom: "1rem"}}>
-            <Input
-              type="email"
-              style={{width: "100%", margin: "auto"}}
-              placeholder="이메일"
-              value={email}
-              onChange={onEmailHandler}
-            />
-          </Col>
-        </Row>
-        <Row className="justify-content-center">
-          <Col style={{marginBottom: "1rem"}}>
-            <Input
-              type="password"
-              style={{width: "100%", margin: "auto"}}
-              placeholder="비밀번호"
-              value={password}
-              onChange={onPasswordHandler}
-            />
-          </Col>
-        </Row>
-        <Row className="justify-content-center">
-          <MyButton
-            style={{width: "50%", marginBottom: "1rem"}}
-            onClick={onSubmit}
-          >
-            로그인
-          </MyButton>
-        </Row>
-
-      </Form>
+    <S.Contents onKeyPress={handleKeyPress}>
+      <Row className="justify-content-center">
+        <Col style={{marginBottom: "1rem"}}>
+          <Input
+            type="email"
+            style={{width: "100%", margin: "auto"}}
+            placeholder="이메일"
+            value={email}
+            onChange={onEmailHandler}
+          />
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col style={{marginBottom: "1rem"}}>
+          <Input
+            type="password"
+            style={{width: "100%", margin: "auto"}}
+            placeholder="비밀번호"
+            value={password}
+            onChange={onPasswordHandler}
+          />
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <MyButton
+          style={{width: "50%", marginBottom: "1rem"}}
+          onClick={onSubmit}
+        >
+          로그인
+        </MyButton>
+      </Row>
     </S.Contents>
   )
 };
