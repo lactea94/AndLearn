21c84bd73b5b8e9e520d0377bd3db46504dde274
dannyp0930 <dannyp0930@gmail.com>
2022-04-07 20:47:01 +0900

    FIX: to article detail link

--- frontend/src/Components/Profile/ProfileArticles/Articles/Articles.js
+++ frontend/src/Components/Profile/ProfileArticles/Articles/Articles.js
@@ -1,16 +1,22 @@
 import { DateFormat } from 'Util/DateFormat';
 import * as S from './Style';
 
-export default function Articles({ articles, offset, limit, me }) {
+export default function Articles({ articles, offset, limit, user }) {
   function ArticleComponent(article, notice) {
-    const articleId = article.id
     return (
       <S.TableRow notice={notice} key={article.id}>
         <S.Column xs={2} md={1}>
-          {notice ? '공지' : `${articleId}`}
+          {notice ? '공지' : `${article.id}`}
         </S.Column>
         <S.Column xs={4} md={7}>
-          {article.title}
+          <S.DetailLink
+            to={`/community/${article.id}`}
+            state={{
+              user: user,
+            }}
+          >
+            {article.title}
+          </S.DetailLink>
         </S.Column>
         <S.Column xs={3} md={2}>{article.nickname}</S.Column>
         <S.Column xs={3} md={2}>
@@ -40,24 +46,6 @@ export default function Articles({ articles, offset, limit, me }) {
       {articles.slice(offset, offset + limit).map((article) => (
         ArticleComponent(article, 0)
       ))}
-
-      {/* {articles.slice(offset, offset + limit).map((article, index) => (
-        <S.TableRow key={article.id}>
-          <S.Column xs={1}>{article.id}</S.Column>
-          <S.Column xs={7}>
-            <S.DetailLink
-              to={`/community/${article.id}`}
-              state={{
-                user: user,
-              }}
-            >
-              {article.title}
-            </S.DetailLink>
-          </S.Column>
-          <S.Column xs={2}>{article.nickname}</S.Column>
-          <S.Column xs={2}>{DateFormat(article.createdDate)}</S.Column>
-        </S.TableRow>
-      ))} */}
     </S.Table>
   )
 };
