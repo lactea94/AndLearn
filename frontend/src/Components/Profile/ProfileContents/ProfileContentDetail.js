66328d72096d9e1703daefea2bcee6031fbeea2a
dannyp0930 <dannyp0930@gmail.com>
2022-04-07 21:43:26 +0900

    STYLE: profile content

--- frontend/src/Components/Profile/ProfileContents/ProfileContentDetail.js
+++ frontend/src/Components/Profile/ProfileContents/ProfileContentDetail.js
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { Row, Col, Image } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import * as S from './ProfileContentDetailStyle';
 import { apiInstance } from "api";
@@ -24,49 +24,46 @@ export function ProfileContentDetail() {
   return (
     <>
       {learningInfo && 
-        <div>
+        <S.Container>
           <Row className="justify-content-center">
-            <Col xs={12} md={6}>
-              <Image 
+            <Col xs={12} md={5}>
+              <S.MyImage 
                 src={`https://d3qljd3xvkb8gz.cloudfront.net/${learningInfo.pictureUrl}`} 
-                alt="profile_image" 
-                rounded 
-                fluid 
-                style={{ width: '100%', marginBottom: '10px', overflow: 'hidden', objectFit: 'cover' }}
+                alt="profile_image"
               />
             </Col>
-            <Col>
+            <Col xs={12} md={5}>
               <Col>
                 <S.Text>학습 날짜 : {learningInfo.createdDate.substr(0, 19)}</S.Text>
                 <S.Text>발음 점수 : {learningInfo.score}</S.Text>
               </Col>
-              <S.AudioBox>
-                <S.Text1>첫 번째 녹음</S.Text1>
-                <audio controls>
-                  <source src={`${learningInfo.records[0].recordUrl}`} />
-                </audio>
-                <S.Text1>{learningInfo.records[0].sentence}</S.Text1>
-              </S.AudioBox>
               <Col>
                 <S.Button color="#58C063" onClick={() => {onToggleOpen()}}>{isCheckAnswer ? '닫기' : 'AI 단어 보기'}</S.Button>
               </Col>
               <S.AnswerBox style={{ display : isCheckAnswer ? '' : 'none'}}>
                 <S.Text1>{learningInfo.words.map(word => word.content).join(", ")}</S.Text1>
               </S.AnswerBox>          
-              <S.AudioBox>
-                <S.Text1>두 번째 녹음</S.Text1>
-                <audio controls>
-                  <source src={`${learningInfo.records[1].recordUrl}`} />
-                </audio>
-                <S.Text1>{learningInfo.records[1].sentence}</S.Text1>
-              </S.AudioBox>
-              <Col>
-                <S.Button onClick={() => navigate(-1)}>뒤로가기</S.Button>
-              </Col>
+            </Col>
+            <S.AudioBox xs={12} md={5}>
+              <S.Text1>첫 번째 녹음</S.Text1>
+              <S.Audio controls>
+                <source src={`${learningInfo.records[0].recordUrl}`} />
+              </S.Audio>
+              <S.Text1>{learningInfo.records[0].sentence}</S.Text1>
+            </S.AudioBox>
+            <S.AudioBox xs={12} md={5}>
+              <S.Text1>두 번째 녹음</S.Text1>
+              <S.Audio controls>
+                <source src={`${learningInfo.records[1].recordUrl}`} />
+              </S.Audio>
+              <S.Text1>{learningInfo.records[1].sentence}</S.Text1>
+            </S.AudioBox>
+            <Col lg={2}>
+              <S.Button onClick={() => navigate(-1)}>뒤로가기</S.Button>
             </Col>
           </Row>
           <br />
-        </div>
+        </S.Container>
       }
     </>
   )
--- frontend/src/Components/Profile/ProfileContents/ProfileContentDetailStyle.js
+++ frontend/src/Components/Profile/ProfileContents/ProfileContentDetailStyle.js
@@ -1,6 +1,20 @@
-import { Col } from "react-bootstrap";
+import { Col, Image } from "react-bootstrap";
 import styled from "styled-components";
 import { MyButton } from "styles/Button";
+
+export const Container = styled.div`
+  background-color: #f8f8f8;
+  padding: 2rem;
+  border-radius: 1rem;
+`
+
+export const MyImage = styled(Image)`
+  display: fluid;
+  width: 100%;
+  margin-bottom: 1rem;
+  overflow: hidden;
+  object-fit: cover;
+`
 
 export const Text = styled.div`
   text-align: left;
@@ -18,12 +32,14 @@ export const AudioBox = styled(Col)`
   margin: 0.5rem;
   padding: 1rem;
   border-radius: 1rem;
+ `
+
+export const Audio = styled.audio`
+  width: 100%;
 `
 
 export const AnswerBox = styled(Col)`
   border: 1px solid gray;
-  margin: 0.5rem;
-  padding: 1rem;
   border-radius: 1rem;
 `
 
