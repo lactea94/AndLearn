85961f8c2dfa0ce35a77772c82ba790b9247348e
dannyp0930 <dannyp0930@gmail.com>
2022-04-07 15:20:35 +0900

    STYLE: content detail

--- frontend/src/Components/Profile/ProfileContents/ProfileContentDetail.js
+++ frontend/src/Components/Profile/ProfileContents/ProfileContentDetail.js
@@ -26,7 +26,7 @@ export function ProfileContentDetail() {
       {learningInfo && 
         <div>
           <Row className="justify-content-center">
-            <Col xs={12}>
+            <Col xs={12} md={6}>
               <Image 
                 src={`https://d3qljd3xvkb8gz.cloudfront.net/${learningInfo.pictureUrl}`} 
                 alt="profile_image" 
@@ -35,34 +35,36 @@ export function ProfileContentDetail() {
                 style={{ width: '100%', marginBottom: '10px', overflow: 'hidden', objectFit: 'cover' }}
               />
             </Col>
-            <Col xs={12}>
-              <S.Text>학습 날짜 : {learningInfo.createdDate.substr(0, 19)}</S.Text>
-              <S.Text>발음 점수 : {learningInfo.score}</S.Text>
-            </Col>
-            <Col xs={12}>
-              <S.Text1>1차 녹음본</S.Text1>
-              <audio controls>
-                <source src={`${learningInfo.records[0].recordUrl}`} />
-              </audio>
-            </Col>
-            <Col xs={12}>
-              <S.Text1>2차 녹음본</S.Text1>
-              <audio controls>
-                <source src={`${learningInfo.records[1].recordUrl}`} />
-              </audio>
-            </Col>
-            <Col xs={12}>
-              <S.Text>AI 단어 / 내 답변</S.Text>
-            </Col>
-            <Col >
-              <S.AnswerButton onClick={() => {onToggleOpen()}}>{isCheckAnswer ? 'Close' : 'Open'}</S.AnswerButton>
-            </Col>
             <Col>
-              <S.AnswerBox style={{ display : isCheckAnswer ? '' : 'none'}}>
-                <S.Text>AI 단어 : {learningInfo.words.map(word => word.content).join(", ")}</S.Text>
-                <S.Text>첫 번째 답변 : {learningInfo.records[0].sentence}</S.Text>
-                <S.Text>두 번째 답변 : {learningInfo.records[1].sentence}</S.Text>
-              </S.AnswerBox>          
+              <Col>
+                <S.Text>학습 날짜 : {learningInfo.createdDate.substr(0, 19)}</S.Text>
+                <S.Text>발음 점수 : {learningInfo.score}</S.Text>
+              </Col>
+              <Col>
+                <S.Text1>1차 녹음본</S.Text1>
+                <audio controls>
+                  <source src={`${learningInfo.records[0].recordUrl}`} />
+                </audio>
+              </Col>
+              <Col>
+                <S.Text1>2차 녹음본</S.Text1>
+                <audio controls>
+                  <source src={`${learningInfo.records[1].recordUrl}`} />
+                </audio>
+              </Col>
+              <Col>
+                <S.Text>AI 단어 / 내 답변</S.Text>
+              </Col>
+              <Col>
+                <S.AnswerButton onClick={() => {onToggleOpen()}}>{isCheckAnswer ? 'Close' : 'Open'}</S.AnswerButton>
+              </Col>
+              <Col>
+                <S.AnswerBox style={{ display : isCheckAnswer ? '' : 'none'}}>
+                  <S.Text>AI 단어 : {learningInfo.words.map(word => word.content).join(", ")}</S.Text>
+                  <S.Text>첫 번째 답변 : {learningInfo.records[0].sentence}</S.Text>
+                  <S.Text>두 번째 답변 : {learningInfo.records[1].sentence}</S.Text>
+                </S.AnswerBox>          
+              </Col>
             </Col>
           </Row>
           <br />
